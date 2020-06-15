1a0bfbfed3a232eead47d7fa3620b8ea251ae35a
Darcy Webber <darcy@quantifish.co.nz>
2020-06-16 11:18:52 +1200

    added penalties to the likelihood table

--- R/om_table_likelihood.R
+++ R/om_table_likelihood.R
@@ -1,34 +1,35 @@
 #' Table of the likelihood components for a specified set of _lab.rep files
 #'
 #' @export
-#' 
-table_likelihood <- function(data.objects)
-{
+#'
+table_likelihood <- function(data.objects) {
+
    nobjects <- length(data.objects)
-   result <- NULL
+   likelihoods <- NULL
+   penalties <- NULL
    objF <- NULL
    sumNLL <- NULL
    sumPEN <- NULL
    steep <- NULL
    M1 <- NULL
    M10 <- NULL
-   for (i in 1:nobjects)
-   {
+
+   for (i in 1:nobjects) {
       xx <- data.objects[[i]]
       sumNLL <- rbind(sumNLL, sum(xx$lnlike))
       sumPEN <- rbind(sumPEN, sum(xx$penal))
       objF <- rbind(objF, xx$ObjF)
       steep <- rbind(steep, xx$steep)
-      result <- rbind(result, xx$lnlike)
-      M1<- rbind(M1, xx$M[2])
+      likelihoods <- rbind(likelihoods, xx$lnlike)
+      penalties <- rbind(penalties, xx$penal)
+      M1 <- rbind(M1, xx$M[2])
       M10 <- rbind(M10, xx$M[11])
-      rownames(result)[i] <- names(data.objects)[i]
+      rownames(likelihoods)[i] <- names(data.objects)[i]
    }
+
    ncols <- ncol(result)
-   result <- cbind(result, sumNLL, sumPEN, objF, steep, M1, M10)
+   result <- cbind(likelihoods, sumNLL, penalties, sumPEN, objF, steep, M1, M10)
    nresults <- ncol(result)
-   #print(nresults)
-   #print(colnames(result))
-   colnames(result)[(nresults-6+1):nresults] <- c("sumNLL","sumPEN","objF","steepness","M1","M10")
+   colnames(result) <- c(colnames(likelihoods), "sumNLL", colnames(penalties), "sumPEN", "objF", "steepness", "M1", "M10")
    return(result)
 }
