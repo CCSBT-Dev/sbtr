4c9f4cef81aa9c2d407aa9d883a771ec1aaa7658
quantifish <darcy@quantifish.co.nz>
2020-10-25 10:42:16 +1300

    adding new Kobe plot to package properly

--- R/calc_MSYstuff.R
+++ R/calc_MSYstuff.R
@@ -10,8 +10,6 @@
 #' @param y the function input
 #' @return something done on y
 #' @author Ana Parma
-#' @examples
-#' MSY.stats <- calc.MSYstuff.R(base2013)
 #' @export
 #'
 calc.MSYstuff.R <- function(data.objects, lev.file = "base.lev", sample = T) {
@@ -53,7 +51,7 @@ calc.MSYstuff.R <- function(data.objects, lev.file = "base.lev", sample = T) {
    output$B10B10msy =  output$B10 / output$B10msy
    output$FFmsy =  output$F / output$Fmsy
 
-   fits=list(output$MSY,output$Fmsy,output$SSBmsy,output$TBmsy)
+   fits=list(output$MSY, output$Fmsy, output$SSBmsy, output$TBmsy)
    #check convergence and set to NA
    id.wrong <-  output$MSY < 1
    output$MSY[id.wrong]=NA
@@ -65,30 +63,30 @@ calc.MSYstuff.R <- function(data.objects, lev.file = "base.lev", sample = T) {
    output$B10B10msy[id.wrong]=NA
 
  # sample over grid
-   if(sample) {
-     lev <- read.table(file=lev.file,colClasses="numeric",sep=" ")
-     nlevs = nrow(lev)    #
+   if (sample) {
+     lev <- read.table(file = lev.file, colClasses = "numeric", sep = " ")
+     nlevs = nrow(lev)
      lev.scens <- as.numeric(paste(lev[,1],lev[,2],lev[,3],lev[,4],lev[,5],lev[,6],lev[,7],sep=""))
      resamps <- match(lev.scens,output$scenario)
    } else {
      resamps<- 1:nobjects
    }
-   MSY = output$MSY[resamps,]
-   Fmsy=output$Fmsy[resamps,]
-   SSBmsy=output$SSBmsy[resamps,]
-   B10msy=output$B10msy[resamps,]
-   TBmsy=output$TBmsy[resamps,]
-   SSB=output$SSB[resamps,]
-   B10=output$B10[resamps,]
-   TOTbio=output$TOTbio[resamps,]
-   FFmsy=output$FFmsy[resamps,]
-   UUmsy = output$UUmsy[resamps,]
-   BBmsy = output$BBmsy[resamps,]
-   B10B10msy = output$B10B10msy[resamps,]
-   scenario = output$scenario[resamps]
-   B0 = output$SSB0[resamps]
-   B.B0 <- SSB/B0       #B0 is a vector and SSB a matrix
-   B10.B0 <- B10/B10[,1]
+   MSY <- output$MSY[resamps,]
+   Fmsy <- output$Fmsy[resamps,]
+   SSBmsy <- output$SSBmsy[resamps,]
+   B10msy <- output$B10msy[resamps,]
+   TBmsy <- output$TBmsy[resamps,]
+   SSB <- output$SSB[resamps,]
+   B10 <- output$B10[resamps,]
+   TOTbio <- output$TOTbio[resamps,]
+   FFmsy <- output$FFmsy[resamps,]
+   UUmsy <- output$UUmsy[resamps,]
+   BBmsy <- output$BBmsy[resamps,]
+   B10B10msy <- output$B10B10msy[resamps,]
+   scenario <- output$scenario[resamps]
+   B0 <- output$SSB0[resamps]
+   B.B0 <- SSB / B0 # B0 is a vector and SSB a matrix
+   B10.B0 <- B10 / B10[,1]
 
    pquantiles <- c(0.05,0.10,0.25,0.5,0.75,0.9,0.95)
    Bquantiles = round(apply(BBmsy,2,quantile,p=pquantiles,na.rm=T),3)
@@ -99,7 +97,6 @@ calc.MSYstuff.R <- function(data.objects, lev.file = "base.lev", sample = T) {
    Fquantiles = round(apply(FFmsy,2,quantile,p=pquantiles,na.rm=T),3)
    B10msyB0quantiles = round(quantile(B10msy[,nyears-1]/B10[,1],p=pquantiles,na.rm=T),3)
    SSBmsyB0quantiles = round(quantile(SSBmsy[,nyears-1]/B0,p=pquantiles,na.rm=T),3)
-
 
  return(list("scenario"=scenario,"MSY"=MSY,"SSBmsy"=SSBmsy,"B10msy"=B10msy,"TBmsy"=TBmsy,"Fmsy"=Fmsy,
    "FFmsy"=FFmsy,"UUmsy"=UUmsy,"SSB"=SSB,"B10plus"=B10,"TOTbio"=TOTbio,"B0"=B0,
